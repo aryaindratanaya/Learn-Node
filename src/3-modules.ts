ba6f8fa3e0e9ef235f169520d78634af09265ca8
Arya Indra Tanaya <aryaindratanaya@gmail.com>
2023-04-01 03:39:51 +0800

    Create course 6 demonstrating `module.exports`

--- src/3-modules.ts
+++ src/3-modules.ts
@@ -1,5 +1,8 @@
 import { john, peter } from "./4-names.js";
 import { sayHi } from "./5-utils.js";
+import data from "./6-alternative-flavor.cjs";
+
+console.log(data);
 
 sayHi("susan");
 sayHi(john);
